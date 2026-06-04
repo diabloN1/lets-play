8df4b8e61449f0534fbe51c95189e333fbec7c35
myacoubi <amineamoune904@gmail.com>
2026-06-04 19:57:09 +0100

    feat(models): Init User and Post model

---
+++ demo/src/main/java/com/letsplay/demo/product/Product.java
@@ -0,0 +1,20 @@
+package com.letsplay.demo.product;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "products")
+public class Product {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String description;
+    private double price;
+
+    private String userId;
+}
