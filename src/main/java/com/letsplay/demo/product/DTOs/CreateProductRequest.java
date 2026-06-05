e2a5dc00e7c2d24c7b8370256f05c4fe99989a3e
myacoubi <amineamoune904@gmail.com>
2026-06-05 18:42:44 +0100

    feat(product): create / update DTOs

---
+++ src/main/java/com/letsplay/demo/product/DTOs/CreateProductRequest.java
@@ -0,0 +1,20 @@
+package com.letsplay.demo.product.DTOs;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateProductRequest(
+
+    @NotBlank
+    @Size(max = 100)
+    String name,
+
+    @NotBlank
+    @Size(max = 1000)
+    String description,
+
+    @DecimalMin(value = "0.0", inclusive = false)
+    double price
+
+) {}
