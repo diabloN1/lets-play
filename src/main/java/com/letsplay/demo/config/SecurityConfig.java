729f181c7bad761ed079cc83dfa0f214ffd2af53
myacoubi <amineamoune904@gmail.com>
2026-06-06 21:43:29 +0100

    feat: Add JWT authentication filter and security configuration updates

--- src/main/java/com/letsplay/demo/config/SecurityConfig.java
+++ src/main/java/com/letsplay/demo/config/SecurityConfig.java
@@ -1,23 +1,34 @@
 package com.letsplay.demo.config;
 
+import com.letsplay.demo.config.jwt.JwtAuthFilter;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/products/**").permitAll()
-                .requestMatchers("/users/**").permitAll()
-                .anyRequest().authenticated()
-            );
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/products").permitAll()
+                        .requestMatchers("/users/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+        ;
 
         return http.build();
     }
