c18d5dab90b4571214ae7a0bedbf2f0db2f7c088
myacoubi <amineamoune904@gmail.com>
2026-06-06 21:45:46 +0100

    fix: Update validation rules, config property, and record data types

--- src/main/java/com/letsplay/demo/auth/DTOs/RegisterRequest.java
+++ src/main/java/com/letsplay/demo/auth/DTOs/RegisterRequest.java
@@ -5,14 +5,15 @@ import jakarta.validation.constraints.*;
 public record RegisterRequest(
 
     @NotBlank
+    @Size(min = 3, max = 25)
     String name,
 
     @Email
     @NotBlank
     String email,
 
-    @Size(min = 8)
     @NotBlank
+    @Size(min = 8, max = 30)
     String password
 
 ) {}
