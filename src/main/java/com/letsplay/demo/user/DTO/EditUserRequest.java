236e455901fe3d1357222c80549227a1971b1582
myacoubi <amineamoune904@gmail.com>
2026-06-08 06:09:13 +0100

    feat: add DTOs for user creation and editing with validation constraints

---
+++ src/main/java/com/letsplay/demo/user/DTO/EditUserRequest.java
@@ -0,0 +1,20 @@
+package com.letsplay.demo.user.DTO;
+
+import com.letsplay.demo.user.Role;
+
+import jakarta.validation.constraints.*;
+
+public record EditUserRequest(
+
+    @NotBlank
+    @Size(min = 3, max = 25)
+    String name,
+
+    @Email
+    @NotBlank
+    String email,
+
+    @NotNull
+    Role role
+
+) {}
