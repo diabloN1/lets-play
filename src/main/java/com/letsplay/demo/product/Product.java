7068288d55c187a32c9f02c58e169cbb8202fd35
myacoubi <amineamoune904@gmail.com>
2026-06-09 00:50:54 +0100

    fix: implement @Field anotation

--- src/main/java/com/letsplay/demo/product/Product.java
+++ src/main/java/com/letsplay/demo/product/Product.java
@@ -2,6 +2,7 @@ package com.letsplay.demo.product;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Data;
 
@@ -16,5 +17,6 @@ public class Product {
     private String description;
     private double price;
 
+    @Field("owner_id")
     private String userId;
 }
